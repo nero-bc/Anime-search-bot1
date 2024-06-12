3b2e3f8636be36e7da8e5b04b20822a8506771fe
Harry78909 <138775342+Harry78909@users.noreply.github.com>
2024-06-12 17:42:42 +0530

    Update helper_functions.py

--- Helper/helper_functions.py
+++ Helper/helper_functions.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client as app, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Helper import formating_results as format
 from database import ConfigDB
