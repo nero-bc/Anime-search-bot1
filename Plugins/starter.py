664b50d2e1d639fe3610f07cb2c70f6ce58ff1ac
Harry78909 <138775342+Harry78909@users.noreply.github.com>
2024-06-12 16:00:10 +0530

    Update starter.py

--- Plugins/starter.py
+++ Plugins/starter.py
@@ -1,24 +1,27 @@
-from Helper.helper import start_text, help_text
-from config import bot, bot_username
-from telethon import events
+from pyrogram import Client
 from database import UsersDB
+from config import bot, bot_username
+from Helper.helper import start_text, help_text
 
 users = UsersDB()
 
-class Start():
+app = Client("my_bot")
 
-    @bot.on(events.NewMessage(pattern=fr"^/start$|^/start@{bot_username}"))
-    async def event_handler_start(event):
-        users.add({"_id": event.chat_id, "username": event.sender.username, "name": f"{event.sender.first_name} {event.sender.last_name}"})
-        await bot.send_message(
-            event.chat_id,
-            start_text,
-            file='https://tenor.com/view/chika-fujiwara-kaguya-sama-love-is-war-anime-wink-smile-gif-18043249'
-        )
+@app.on_message(filters.command(["start", "start@" + bot_username]))
+async def start_command(client, message):
+    users.add({"_id": message.chat.id, "username": message.from_user.username, "name": f"{message.from_user.first_name} {message.from_user.last_name}"})
+    await client.send_message(
+        message.chat.id,
+        start_text,
+        file='https://tenor.com/view/chika-fujiwara-kaguya-sama-love-is-war-anime-wink-smile-gif-18043249'
+    )
 
-    @bot.on(events.NewMessage(pattern=fr"^/help$|^/help@{bot_username}"))
-    async def event_handler_help(event):
-        await bot.send_message(
-            event.chat_id,
-            help_text
-            )
+@app.on_message(filters.command(["help", "help@" + bot_username]))
+async def help_command(client, message):
+    await client.send_message(
+        message.chat.id,
+        help_text
+    )
+
+if __name__ == '__main__':
+    app.run()
